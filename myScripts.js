98bf9b8f71b85a91ba81b8e0485ae10f6f711f20
bwittmer <bwittmer@bsu.edu>
2021-03-04 19:35:45 -0500

    Update myScripts.js

--- myScripts.js
+++ myScripts.js
@@ -1,14 +1,14 @@
 //Get the button:
-mybutton = document.getElementById("myBtn");
+var button = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    button.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    button.style.display = "none";
   }
 }
 
